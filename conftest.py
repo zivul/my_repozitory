35f34844cfb4286c63d2b769bc6e36152b2cf58e
Vadik <analiz-k@mail.ru>
2024-10-19 12:13:42 +0300

    only

--- conftest.py
+++ conftest.py
@@ -2,12 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+
 
 @pytest.fixture()
 def set_up():
     options = Options()
-    driver = webdriver.Chrome(options = options)
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(60)
     driver.maximize_window()
     yield driver
--- test/test_apteka.py
+++ test/test_apteka.py
@@ -1,15 +1,58 @@
 from conftest import *
 
-class TestResult:
-    def test_apt(self, set_up):
+
+class TestOnly:
+    def test_footer(self, set_up):
         driver = set_up
-        driver.get('https://apteka-altay.ru/')
-        el = driver.find_element(By.XPATH, '//*[text()="Товары по назначению"]').text
-        assert el == 'Товары по назначению'
+        driver.get('https://only.digital/')
+        try:
+            driver.find_element(By.XPATH, '//footer')
+            print("Element found")
+        except NoSuchElementException:
+            print("No element found")
+
+    def test_footer_project(self, set_up):
+        driver = set_up
+        driver.get('https://only.digital/projects')
+        try:
+            driver.find_element(By.XPATH, '//footer')
+            print("Element found")
+        except NoSuchElementException:
+            print("No element found")
+
+    def test_footer_el1(self, set_up):
+        driver = set_up
+        driver.get('https://only.digital/')
+        try:
+            driver.find_element(By.XPATH, '//p[@class="sc-222969c7-1 eNylzl"]')
+            print("Element found")
+        except NoSuchElementException:
+            print("No element found")
+
+    def test_footer_el2(self, set_up):
+        driver = set_up
+        driver.get('https://only.digital/')
+        el = driver.find_element(By.XPATH, '(//a[@rel="noreferrer"])[7]')
+        assert '' == el.text
+
+    def test_footer_el3(self, set_up):
+        driver = set_up
+        driver.get('https://only.digital/')
+        el = driver.find_element(By.XPATH, "(//span[contains(text(), 'Awwwards')])[2]").text
+        assert el == ''
+
+    def test_footer_el4(self, set_up):
+        driver = set_up
+        driver.get('https://only.digital/')
+        try:
+            driver.find_element(By.XPATH, "(//span[contains(text(), 'Awwwards')])[2]")
+            print("Element found")
+        except NoSuchElementException:
+            print("No element found")
 
 
-    def test_apt_2(self, set_up):
-        driver = set_up
-        driver.get('https://apteka-altay.ru/zhivica')
-        el2 = driver.find_elements(By.CLASS_NAME, 'product-thumb')
-        assert len(el2) == 15
+
+
+
+
+
